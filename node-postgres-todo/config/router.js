941342f2b86e6ae7e1f3727b60136d6ea8a5d267
christine juruena <christinemaejuruena@gmail.com>
2015-11-05 18:14:25 +0800

    Initial project stuff

---
+++ node-postgres-todo/config/router.js
@@ -0,0 +1,32 @@
+var getget = require('');
+
+router.get('/api/v1/todos', function(req, res) {
+
+    var results = [];
+
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+        // Handle connection errors
+        if(err) {
+          done();
+          console.log(err);
+          return res.status(500).json({ success: false, data: err});
+        }
+
+        // SQL Query > Select Data
+        var query = client.query("SELECT * FROM items ORDER BY id ASC;");
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            done();
+            return res.json(results);
+        });
+
+    });
+
+});
