7641307520ad393f0135e5ae597eb4f8c5654503
christine juruena <christinemaejuruena@gmail.com>
2015-11-17 20:24:10 +0800

    removed stuff

--- spotify (copy)/config.js
+++
@@ -1,4 +0,0 @@
-var connectionString = process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/spotify';
-// var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
-
-module.exports = connectionString;
